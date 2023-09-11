fd617da19dc51d36a08140ddbc5a925771b08095
Andrii Hutnyk <andreigutnik@gmail.com>
2023-09-11 20:03:08 +0200

    Create template

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,33 @@
+import { useState } from 'react';
+import { GlobalStyle } from './GlobalStyle';
+import { Layout } from './Layout';
+import { ScrollUp } from './ScrollUp/ScrollUp';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { animateScroll as scroll } from 'react-scroll';
+
 export const App = () => {
+  const [isScrollUp, setisScrollUp] = useState(false);
+
+  const onScroll = () => {
+    const scrollY = window.scrollY || document.documentElement.scrollTop;
+    if (scrollY > 300) {
+      setisScrollUp(true);
+    } else {
+      setisScrollUp(false);
+    }
+  };
+
+  const onScrollUp = () => {
+    scroll.scrollToTop();
+    setisScrollUp(false);
+  };
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Layout>
+      {isScrollUp && <ScrollUp onClick={onScrollUp} />}
+      <ToastContainer autoClose={3000} />
+      <GlobalStyle />
+    </Layout>
   );
 };
---
+++ src/components/Layout.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const Layout = styled.main`
+  width: 1440px;
+  padding: 0 12px 36px;
+  margin: 0 auto;
+`;
